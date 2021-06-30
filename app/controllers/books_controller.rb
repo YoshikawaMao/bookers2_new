1e213a20a603f1171cb10a68d12ad8efc9feb79b
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-06-30 12:17:59 +0000

    [add]book/indexページ、create機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,61 @@
 class BooksController < ApplicationController
-  def index
-  end
+     def index
+       @book = Book.new
+       @books = Book.all
+       @user = current_user
+     end
 
-  def show
-  end
+     def show
+         @books = Book.all
+         @book = Book.find(params[:id])
+         @user = @book.user
+         @book_new = Book.new
+     end
 
-  def edit
-  end
+     def create
+      # １. データを新規登録するためのインスタンス作成
+      @user = current_user
+      @books = Book.all
+      @book = Book.new(book_params)
+      @book.user_id = current_user.id
+      # ２. データをデータベースに保存するためのsaveメソッド実行
+      if @book.save
+         flash[:notice] = "You have create book successfully."
+         # ３.showページに飛ぶ
+         redirect_to book_path(@book)
+      else
+         render :index
+      end
+     end
+
+
+     def edit
+         @book = Book.find(params[:id])
+         if @book.user == current_user
+              render "edit"
+         else
+             redirect_to books_path
+         end
+     end
+
+     def update
+         @book = Book.find(params[:id])
+         if @book.update(book_params)
+             flash[:notice] = "You have updated book successfully."
+             redirect_to book_path(@book)
+         else
+             render :edit
+         end
+     end
+
+     def destroy
+         book = Book.find(params[:id])  # データ（レコード）を1件取得
+         book.destroy  # データ（レコード）を削除
+         redirect_to books_path  # 投稿一覧画面へリダイレクト
+     end
+
+     private
+     def book_params
+          params.require(:book).permit(:title, :body)
+     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,37 @@
 class UsersController < ApplicationController
-  def show
-  end
+   def index
+      @user = current_user
+      @book = Book.new
+      @users = User.all
+   end
 
-  def index
-  end
+   def show
+      @user = User.find(params[:id])
+      @book_new = Book.new
+      @books = @user.books
+   end
 
-  def edit
-  end
+   def edit
+       @user = User.find(params[:id])
+       if @user == current_user
+         render "edit"
+       else
+         redirect_to user_path(current_user)
+       end
+   end
+
+   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+       flash[:notice] = "You have update user successfully"
+       redirect_to user_path(@user)
+    else
+         render :edit
+    end
+   end
+
+   private
+     def user_params
+          params.require(:user).permit(:name, :introduction, :profile_image)
+     end
 end
